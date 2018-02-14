1d6aca57489c9d78a2403ba24345b386224c443c
NJULeafeon <maoym@126.com>
2018-02-14 14:49:13 +0800

    2-9

---
+++ 2-9 search-ex/cmake-build-debug/CMakeFiles/2_9_search_ex.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/2_9_search_ex.dir/main.cpp.o"
+  "2_9_search_ex.pdb"
+  "2_9_search_ex.exe"
+  "lib2_9_search_ex.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_9_search_ex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
