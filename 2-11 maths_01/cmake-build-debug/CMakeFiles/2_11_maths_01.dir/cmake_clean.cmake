f0386d7981e9e2b232ce6da96cf3a482b7910959
NJULeafeon <maoym@126.com>
2018-02-21 22:27:24 +0800

    2-11

---
+++ 2-11 maths_01/cmake-build-debug/CMakeFiles/2_11_maths_01.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/2_11_maths_01.dir/main.cpp.o"
+  "2_11_maths_01.pdb"
+  "2_11_maths_01.exe"
+  "lib2_11_maths_01.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_11_maths_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
