d33505eb38817823a08bdc297b7b8f996b61a698
NJULeafeon <maoym@126.com>
2018-02-24 00:18:59 +0800

    2-13

---
+++ 2-13 linear_data_structure/cmake-build-debug/CMakeFiles/2_13_linear_data_structure.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/2_13_linear_data_structure.dir/main.cpp.o"
+  "2_13_linear_data_structure.pdb"
+  "2_13_linear_data_structure.exe"
+  "lib2_13_linear_data_structure.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_13_linear_data_structure.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
